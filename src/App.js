e0344e7b35c755201ee5182c4f2ed2b2e3348cdf
beejayarts <beejayarts@gmail.com>
2021-02-23 13:59:45 +0000

    my first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import data from "./data";
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if(count<=0){
+       amount = 1;
+    }
+
+    if(count>8){
+       amount = 8;
+    }
+    setText(data.splice(0,amount))
+    console.log(amount)
+  };
+
+  return (
+    <section className="section-center">
+      <h3>Generate interesting Lorem</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          onChange={(e) => setCount(e.target.value)}
+          value={count}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((item, index)=>{
+          return(
+            <p key={index}>{item}</p>
+          )
+        })}
+        
+      </article>
+    </section>
+  );
+}
+
+export default App;
